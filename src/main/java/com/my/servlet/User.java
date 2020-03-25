a2ffb82ab1a062fd42f51b4a635d2d1ab089b205
enotMain <777_Gaga@mail.ru>
2020-03-25 12:43:19 +0300

    Nothing

--- src/main/java/com/my/servlet/User.java
+++ src/main/java/com/my/servlet/User.java
@@ -2,6 +2,9 @@ package com.my.servlet;
 
 import java.io.Serializable;
 
+/**
+ * Класс пользователя
+ */
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
